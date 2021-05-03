91353e2a5515f16198ab6526c1921fc531fad559
coll-j <zumazaki@gmail.com>
2021-05-03 12:42:41 +0700

    threading

--- Tugas2/server/http-simple-server-threading.py
+++ Tugas2/server/http-simple-server-threading.py
@@ -4,20 +4,42 @@ import sys
 import os
 import re
 import threading
+import queue
 
 class serverThread (threading.Thread):
-   def __init__(self, sock):
-      threading.Thread.__init__(self)
-      self.sock = sock
-   def run(self):
-      server(self.sock)
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.running = True
+        self.q = queue.Queue()
 
-def server(sock):
-    # receive data from client, break when null received          
-    data = sock.recv(4096)
-    # print('data ', data)
-    # if data is None:
-        # print('test')
+    def add(self, data, sock):
+        print('adding data')
+        self.q.put((data, sock))
+
+    def stop(self):
+        self.running = False
+
+    def run(self):
+        q = self.q
+        # print('nihao')
+        while self.running:
+            try:
+                # block for 1 second only:
+                value = q.get(block=True, timeout=1)
+                # print('got data')
+                print(value)
+                process(value[0], value[1])
+            except queue.Empty:
+                sys.stdout.write('.')
+                sys.stdout.flush()
+
+        if not q.empty():
+            print("Elements left in the queue:")
+            while not q.empty():
+                print(q.get())
+        # self.server(self.sock)
+
+def process(data, sock):
     data = data.decode('utf-8')
     # print(data)
     
@@ -104,12 +126,17 @@ def server(sock):
             response_header = 'HTTP/1.1 200 OK \r\nContent-Disposition: attachment; filename="' + request_file + '"\r\nContent-Type: application/octet-stream\r\n \
                 Content-Length:' \
                             + str(content_length) + '\r\n\r\n'
-        print('tes ')
+        # print('tes ', response_header)
         sock.sendall(response_header.encode('utf-8') + response_data)
         sock.sendall(b'')
         sock.shutdown(socket.SHUT_RDWR)
         sock.close()
     return
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
 
 def getPort(config):
     try:
@@ -124,10 +151,14 @@ def find_file(filename):
             # print(f)
             # print(filename)
             if filename == f:
-                print('test', root)
+                # print('test', root)
                 return os.path.join(root, f)
 
     return None
+
+def cleanup():
+    t.stop()
+    t.join()
 
 config_path = os.path.join('server', 'httpserver.conf')
 with open(config_path) as config:
@@ -140,7 +171,12 @@ with open(config_path) as config:
         print('Port configuration not found on file: {}'.format(config_path))
         sys.exit(1)
 
-server_address = ('127.0.0.1', port)
+t = serverThread()
+t.start()
+
+ip_address = get_ip_address()
+print('ip: ', ip_address)
+server_address = (ip_address, port)
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(server_address)
@@ -150,19 +186,36 @@ input_socket = [server_socket]
 
 while True:
     try:
-        read_ready, write_ready, exception = select.select(input_socket, [], [])
+        # print('tes')
+        client, address = server_socket.accept()
+        read_ready, write_ready, exception = select.select([client, ], [], [])
         
-        for sock in read_ready:
-            if sock == server_socket:
-                client_socket, client_address = server_socket.accept()
-                input_socket.append(client_socket)                       
+        if read_ready[0]:
+            data = client.recv(4096)
+            t.add(data, client)
+        # print('wokow')
+        # for sock in read_ready:
+        #     print('halo')
+        #     if sock == server_socket:
+        #         print('hehe')
+        #         client_socket, client_address = server_socket.accept()
+        #         input_socket.append(client_socket)                       
             
-            else:  
-                server = serverThread(sock)    
-                input_socket.remove(client_socket)
+        #     else:  
+        #         # serverThread(sock)
+        #         print('tus')    
+        #         data = sock.recv(4096)
+        #         t.add(data, sock)
+        #         client_socket.shutdown(socket.SHUT_RDWR)
+        #         client_socket.close()
+        #         input_socket.remove(client_socket)
 
     except KeyboardInterrupt:        
         server_socket.close()
-        sys.exit(0)
-    except:
+        # sys.exit(0)
+        break
+    except Exception as e:
+        print((str(e)))
         pass
+cleanup()
+sys.exit(0)
