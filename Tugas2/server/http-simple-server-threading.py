9882abc3f70e28a069e80c3a52a605e454098de6
coll-j <zumazaki@gmail.com>
2021-05-03 12:59:52 +0700

    bug fixed

--- Tugas2/server/http-simple-server-threading.py
+++ Tugas2/server/http-simple-server-threading.py
@@ -71,103 +71,67 @@ def process(data, sock):
 
         sock.sendall(response_header.encode('utf-8') + response_data.encode('utf-8'))
     elif request_file == 'dataset' or request_file == '/dataset':
+        print('dataset requested')
         fname = os.path.join('server', 'dataset')
 
-        # receive data from client, break when null received          
-        data = sock.recv(4096)
-        # print('data ', data)
-        # if data is None:
-            # print('test')
-        data = data.decode('utf-8')
-        # print(data)
-        
-        request_header = data.split('\r\n')
-        # print(request_header)
-        if len(request_header[0]) < 1:
-            sock.shutdown(socket.SHUT_RDWR)
-            sock.close()
-            return
-        print(request_header[0])
-        request_file = re.findall(r"GET /([A-Za-z0-9\.'%20'\-]*)", request_header[0])
-        print(request_file)
-        
-        try:
-            request_file = ' '.join(request_file[0].split('%20'))
-        except:
-            request_file = ''
-        response_header = b''
-        response_data = b''
-        
-        print('req: ', request_file)
-        if request_file == 'index.html' or request_file == '':
-            fname = os.path.join('server', 'index.html')
-            f = open(fname, 'r')
+        list_files = os.listdir(fname)
+        print('got list files')
+        list_files.sort(key=lambda a: a.lower())
+        r = []
+        r.append('<!DOCTYPE HTML>')
+        r.append('<html>\n<head>')
+        r.append('<meta http-equiv="Content-Type" '
+                'content="text/html"; charset="UTF-8">')
+        r.append('<title>This is a tile</title>\n</head>' )
+        r.append('<body>\n<h1>This is a tile</h1>')
+        r.append('<hr>\n<ul>')
+        for name in list_files:
+            fullname = os.path.join(fname, name)
+            displayname = linkname = name
+            # Append / for directories or @ for symbolic links
+            if os.path.isdir(fullname):
+                displayname = name + "/"
+                linkname = name + "/"
+            if os.path.islink(fullname):
+                displayname = name + "@"
+                # Note: a link to a directory displays with @ and links with /
+            r.append('<li><a href="%s">%s</a></li>'
+                    % (linkname,displayname))
+        r.append('</ul>\n<hr>\n</body>\n</html>\n')
+
+        response_data = ''.join(r)
+        content_length = len(response_data)
+        print(response_data)
+        response_header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length:' \
+                            + str(content_length) + '\r\n\r\n'
+        sock.sendall(response_header.encode('utf-8') + response_data.encode('utf-8'))
+    else:
+        print('any other')
+        # sock.sendall(b'HTTP/1.1 404 Not found\r\n\r\n')
+        filepath = find_file(request_file)
+        print('fpath; ', filepath )
+        if filepath is None:
+            fname = os.path.join('server', '404.html')
+            f = open(fname, 'rb')
             response_data = f.read()
             f.close()
             
             content_length = len(response_data)
-            response_header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length:' \
-                                + str(content_length) + '\r\n\r\n'
-
-            sock.sendall(response_header.encode('utf-8') + response_data.encode('utf-8'))
-        elif request_file == 'dataset' or request_file == '/dataset':
-            fname = os.path.join('server', 'dataset')
-
-            list = os.listdir(fname)
-            list.sort(key=lambda a: a.lower())
-            r = []
-            r.append('<!DOCTYPE HTML>')
-            r.append('<html>\n<head>')
-            r.append('<meta http-equiv="Content-Type" '
-                    'content="text/html"; charset="UTF-8">')
-            r.append('<title>This is a tile</title>\n</head>' )
-            r.append('<body>\n<h1>This is a tile</h1>')
-            r.append('<hr>\n<ul>')
-            for name in list:
-                fullname = os.path.join(fname, name)
-                displayname = linkname = name
-                # Append / for directories or @ for symbolic links
-                if os.path.isdir(fullname):
-                    displayname = name + "/"
-                    linkname = name + "/"
-                if os.path.islink(fullname):
-                    displayname = name + "@"
-                    # Note: a link to a directory displays with @ and links with /
-                r.append('<li><a href="%s">%s</a></li>'
-                        % (linkname,displayname))
-            r.append('</ul>\n<hr>\n</body>\n</html>\n')
-
-            response_data = ''.join(r)
-            content_length = len(response_data)
-            print(response_data)
-            response_header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length:' \
-                                + str(content_length) + '\r\n\r\n'
-            sock.sendall(response_header.encode('utf-8') + response_data.encode('utf-8'))
+            response_header = 'HTTP/1.1 404 NOT FOUND\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length:' \
+                            + str(content_length) + '\r\n\r\n'
+            sock.sendall(response_header.encode('utf-8') + response_data)
+            print('done sending')
         else:
-            # sock.sendall(b'HTTP/1.1 404 Not found\r\n\r\n')
-            filepath = find_file(request_file)
-            print('fpath; ', filepath )
-            if filepath is None:
-                fname = os.path.join('server', '404.html')
-                f = open(fname, 'rb')
-                response_data = f.read()
-                f.close()
-                
-                content_length = len(response_data)
-                response_header = 'HTTP/1.1 404 NOT FOUND\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length:' \
-                                + str(content_length) + '\r\n\r\n'
-                sock.sendall(response_header.encode('utf-8') + response_data)
-            else:
-                fname = filepath
-                f = open(fname, 'rb')
-                response_data = f.read()
-                f.close()
-                content_length = len(response_data)
+            fname = filepath
+            f = open(fname, 'rb')
+            response_data = f.read()
+            f.close()
+            content_length = len(response_data)
 
             response_header = 'HTTP/1.1 200 OK \r\nContent-Disposition: attachment; filename="' + request_file + '"\r\nContent-Type: application/octet-stream\r\n \
                 Content-Length:' \
                             + str(content_length) + '\r\n\r\n'
-        # print('tes ', response_header)
+        print('tes ', response_header)
         sock.sendall(response_header.encode('utf-8') + response_data)
         sock.sendall(b'')
         sock.shutdown(socket.SHUT_RDWR)
