512e63e92fc2c1ef4ee62d394c63ed450f0229f2
Yodhan <jordanrilbert@gmail.com>
2021-07-04 23:42:30 +0700

    merging chat with game

--- Final-project/client.py
+++ Final-project/client.py
@@ -89,8 +89,127 @@ def start_game():
 
     sys.exit(0)
 
+#----------------------------------------
+
+def read_msg_chat(sock_cli):
+    while True:
+        # terima pesan
+        data = sock_cli.recv(65535)
+        if len(data) == 0:
+            break
+
+        msg = data.decode('utf-8')
+        if '<' in msg:
+            clear_line()
+            print(msg)
+            print("Pilih aksi [1: kirim pesan, 2: kirim file, 3: lihat daftar pengguna, 4: tambah teman, 5: exit]:")
+        elif msg == "file":
+            clear_line()
+            data = sock_cli.recv(65535)
+            msg = data.decode('utf-8')
+            print("file recieve :" + msg)
+            client_recieve_file(sock_cli,msg)
+        else:
+            clear_line()
+            print(msg)
+
+def clear_line():
+    sys.stdout.write("\033[F") #back to previous line 
+    sys.stdout.write("\033[K") #clear line
+
+def client_recieve_file(client_socket,filename):
+    size = client_socket.recv(65535)
+    size = size.decode('utf-8')
+    size = int(float(size))
+    size += 1
+    file = open(filename, 'wb')
+    while size> 0:
+        data = client_socket.recv(65535)
+        file.write(data)
+        size -= 1
+    file.close()
+
+def client_send_file(client_socket,filename):
+    file = open(filename)
+    file.seek(0, os.SEEK_END)
+    filesize =str(file.tell()/65535)
+    client_socket.send(bytes(filesize, "utf-8"))
+    file.close()
+
+    file = open(filename,'rb')
+
+    while True:
+        data = file.read(65535)
+        if not data:
+            file.close()
+            break
+        client_socket.send(data)
+
+
+
 def start_chat():
-    print(5)
+    try:
+        # buat object socket
+        sock_cli = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # connect ke server
+        sock_cli.connect(("127.0.0.1", 6667))
+
+        # kirim username ke server
+        username = sys.argv[1]
+        # data = "{}".format(username)
+        sock_cli.send(bytes(username, "utf-8"))
+
+        thread_cli = threading.Thread(target=read_msg_chat, args=(sock_cli,))
+        thread_cli.start()
+
+        while True:
+            act = int(input("Pilih aksi [1: kirim pesan, 2: kirim file, 3: lihat daftar pengguna, 4: tambah teman, 5: exit]:\n"))
+            clear_line()
+            if act == 1:
+                clear_line()
+                # kirim/terima pesan
+                dest = input("Masukkan username tujuan (ketikan bcast untuk broadcast pesan):")
+                clear_line()
+                msg = input("Masukkan pesan untuk {}:".format(dest))
+                clear_line()
+                data = ["chat",username, dest, msg]
+
+                print("<{}>: {}".format(username, msg))
+                sock_cli.send(bytes('|'.join(data), 'utf-8'))
+            elif act == 2:
+                clear_line()
+                # kirim/terima file
+                dest = input("Masukkan username tujuan (ketikan bcast untuk broadcast file):")
+                clear_line()
+                msg = input("Masukkan path file untuk {}:".format(dest))
+                clear_line()
+                if(os.path.isfile(msg)):
+                    data = ["file",username, dest, msg]
+
+                    print("<{}>: {}".format(username, msg))
+                    sock_cli.send(bytes('|'.join(data), 'utf-8')) 
+                    client_send_file(sock_cli,msg)
+                else:
+                    print("file not found")
+            elif act == 3:
+                # lihat daftar pengguna
+                # clear_line()
+                sock_cli.send(bytes('get_user', 'utf-8'))
+            elif act == 4:
+                # tambah teman
+                clear_line()
+                dest = input("Masukkan username yang ingin ditambahkan:")
+                # clear_line()
+                
+                data = "add|{}|{}".format(username, dest)
+                sock_cli.send(bytes(data, 'utf-8'))
+            if act == 5:
+                # sock_cli.send(bytes('exit', 'utf-8'))
+                sock_cli.close()
+                break
+    except:
+        pass
     
 def start_app():
     thread_game = threading.Thread(target=start_game, args=())
@@ -98,7 +217,6 @@ def start_app():
     # wait for the game to start first
     while wait_chat:
         time.sleep(1)
-    print(wait_chat)
     start_chat()
     # thread_chat = threading.Thread(target=start_chat, args=())
     # thread_chat.start()
--- Final-project/server.py
+++ Final-project/server.py
@@ -99,13 +99,165 @@ def start_server_game():
         for t in threads:
             t.join()
         sys.exit(0)
+
+#-------------------------------------------
+
+def read_msg_chat(clients, friends, sock_cli, addr_cli, username_cli):
+    while True:
+        # terima pesan
+        data = sock_cli.recv(65535)
+        if len(data) == 0:
+            break
+        
+        # parsing pesannya
+        act = data.decode("utf - 8").split("|")[0]
+
+        if act == 'get_user':
+            send_user_list(clients, sock_cli)
+        elif act == 'add':
+            _, user_1, user_2 = data.decode("utf - 8").split("|")
+            if user_2 in clients:
+                add_friend(friends, user_1, user_2)
+                send_msg(sock_cli, '{} added as friend'.format(user_2))
+            else:
+                send_msg(sock_cli, '{} not found'.format(user_2))
+
+        elif act == 'chat':
+            act, sender, dest, msg = data.decode("utf - 8").split("|")
+            msg = "<{}>: {}".format(username_cli, msg)
+
+            #terusankan psan ke semua klien
+            if dest =="bcast":
+                send_broadcast(clients, friends, '[bcast] ' + msg, addr_cli, sender)
+            else:
+                if dest in clients:
+                    if dest in friends[sender]:
+                        dest_sock_cli = clients[dest][0]
+                        send_msg(dest_sock_cli, msg)
+                    else:
+                        send_msg(sock_cli, '{} is not a friend yet'.format(dest))
+                else:
+                    send_broadcast(clients, friends, '[bcast] ' + msg, addr_cli, sender)
+            print(data)
+        elif act == 'file':
+            act, sender, dest, msg = data.decode("utf - 8").split("|")
+            filename = msg
+            server_recieve_file(sock_cli,filename)
+            # terusankan file ke semua klien
+            if dest =="bcast":
+                send_broadcast(clients, friends,"file" , addr_cli, sender)
+                send_file_broadcast(clients, friends,filename , addr_cli, sender)
+            else:
+                if dest in clients:
+                    if dest in friends[sender]:
+                        dest_sock_cli = clients[dest][0]
+                        send_msg(dest_sock_cli, "file")
+                        server_send_file(dest_sock_cli, filename)
+                    else:
+                        send_msg(sock_cli, '{} is not a friend yet'.format(dest))
+                else:
+                    send_broadcast(clients, friends, "file" , addr_cli, sender)
+                    send_file_broadcast(clients, friends,filename, addr_cli, sender)
+            print(data)
+            os.remove(filename)
+    sock_cli.close()
+    print("Connection closed", addr_cli)
+
+# kirim ke semua klien
+def send_broadcast(clients, friends, data, sender_addr_cli, sender_uname):
+    for uname in friends[sender_uname]:
+        sock_cli, addr_cli, _ = clients[uname]
+        if not (sender_addr_cli[0] == addr_cli[0] and sender_addr_cli[1] == addr_cli[1]):
+            send_msg(sock_cli, data)
+
+def send_msg(sock_cli, data):
+    sock_cli.send(bytes(data, "utf-8"))
+
+def send_user_list(clients, sock_cli):
+    send_msg(sock_cli, ', '.join(clients.keys()))
+
+def add_friend(friends, username_cli, username_friend):
+    friends[username_cli].append(username_friend)
+
+def server_recieve_file(server_socket,filename):
+
+    size = server_socket.recv(65535)
+    size = size.decode('utf-8')
+    size = int(float(size))
+    size += 1
+    file = open(filename, 'wb')
+    while size > 0:
+        data = server_socket.recv(65535)
+        file.write(data)
+        size -= 1
+    file.close()
+
+def server_send_file(server_socket,filename):
+    server_socket.send(bytes(filename,"utf-8"))
+
+    file = open(filename)
+    file.seek(0, os.SEEK_END)
+    filesize =str(file.tell()/65535)
+    server_socket.send(bytes(filesize,"utf-8"))
+    file.close()
+
+    file = open(filename,'rb')
+    while True:
+        data = file.read(65535)
+        if not data:
+            file.close()
+            break
+        server_socket.send(data)
+
+def send_file_broadcast(clients, friends, filename, sender_addr_cli, sender_uname):
+    for uname in friends[sender_uname]:
+        sock_cli, addr_cli, _ = clients[uname]
+        if not (sender_addr_cli[0] == addr_cli[0] and sender_addr_cli[1] == addr_cli[1]):
+            server_send_file(sock_cli, filename)
+
+def start_chat():
+    try:
+        # buat object socket server
+        sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # buat object socket server
+        sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # binding object socket ke alamat IP dan port tertentu
+        sock_server.bind(("0.0.0.0", 6667))
+
+        # listen for an incoming connection
+        sock_server.listen(5)
+
+        # buat dictionary utk menyimpan informasi ttg klien
+        clients = {}
+        friends = {}
+
+        while True:
+            # accept connection dari klien
+            sock_cli, addr_cli = sock_server.accept()
+
+            # baca username klien
+            username_cli = sock_cli.recv(65535).decode("utf-8")
+            print(username_cli, " joined")
+
+            # buat thread baru untuk membaca pesan dan jalankan threadnya
+            thread_cli = threading.Thread(target=read_msg_chat, args=(clients, friends, sock_cli, addr_cli, username_cli))
+            thread_cli.start()
+
+            # simpan informasi ttg klien ke dictionary
+            clients[username_cli] = (sock_cli, addr_cli, thread_cli)
+            friends[username_cli] = []
+    except:
+        pass
+
+
+
 # buat dictionary utk menyimpan informasi ttg klien
 players = {} # {'username': (sock_cli, addr_cli, thread_cli)}
 rooms = {} # {room_number: room_object}
 
-def start_chat():
-    print(5)
-    
+
 def start_app():
     thread_game = threading.Thread(target=start_server_game, args=())
     thread_game.start()
